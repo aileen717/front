16258a2df63d4c1de820602d5c60f04a327302be
aileen717 <tenorioaileen07@gmail.com>
2024-06-26 11:46:18 +0800

    dashboard and menu

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,12 @@
 import 'package:appkctechast/home.dart';
+import 'package:appkctechast/pages/dashboard.dart';
+import 'package:appkctechast/pages/menu.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(MaterialApp(
-    home: Home(),
+   routes: {
+       '/' : (context) => Dashboard(),
+       '/menu' : (context) => Menu(),
+       '/profile' : (context) => Home(),
+   },
 ));
