a060de621c1a559e04f44998282ace14fcf878ee
aileen717 <tenorioaileen07@gmail.com>
2024-06-26 10:06:56 +0800

    update stateful widget

--- lib/home.dart
+++ lib/home.dart
@@ -1,31 +1,37 @@
 import 'package:flutter/material.dart';
 
-class Home extends StatelessWidget {
-const Home ({super.key});
+class Home extends StatefulWidget {
+  const Home ({super.key});
 
-@override
-Widget build(BuildContext context) {
-return Scaffold(
-appBar: AppBar(
-title: Text('User Profile'),
-backgroundColor: Colors.pink[500],
-centerTitle: true,
-),
-body: Padding(
-padding: EdgeInsets.all(20.0),
-child: Column(
-  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-  children: <Widget>[
-    Column(
-    crossAxisAlignment: CrossAxisAlignment.start,
-    children: <Widget>[
-      Center(
-        child: CircleAvatar(
-          child: Image.asset('assets/human.png'),
-          radius: 75.0,
+  @override
+  State<Home> createState() => _HomeState();
+}
 
-        ),
+class _HomeState extends State<Home> {
+  int year = 0;
+
+ @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('User Profile'),
+        backgroundColor: Colors.pink[500],
+        centerTitle: true,
       ),
+      body: Padding(
+        padding: EdgeInsets.all(20.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: <Widget>[
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Center(
+                  child: CircleAvatar(
+                    child: Image.asset('assets/human.png'),
+                    radius: 75.0,
+                  ),
+                ),
     Divider(height: 55.0,
     color: Colors.pink,
     ),
@@ -38,80 +44,88 @@ child: Column(
         SizedBox(width: 3.0,),
         Text(
         'NAME',
-        style: TextStyle(
-        fontSize: 20.0,
-        letterSpacing: 2.0,
-        ),
+          style: TextStyle(
+            fontSize: 20.0,
+            letterSpacing: 2.0,
+          ),
         ),
       ],
     ),
     Text(
-    'Aileen Tenorio',
-    style: TextStyle(
-    fontWeight: FontWeight.bold,
-    fontSize: 25.0,
-    ),
-    ),
+      'Aileen Tenorio',
+        style: TextStyle(
+          fontWeight: FontWeight.bold,
+          fontSize: 25.0,
+        ),
+      ),
     SizedBox(height: 30.0,),
     Row(
       children: [
         Icon(Icons.calendar_month,
             color: Colors.pink,
         ),
+
         SizedBox(width: 3.0,),
         Text(
-        'YEAR',
-        style: TextStyle(
-        fontSize: 20.0,
-        letterSpacing: 2.0,
-        ),
+          'YEAR',
+            style: TextStyle(
+              fontSize: 20.0,
+              letterSpacing: 2.0,
+          ),
         ),
       ],
     ),
     Text(
-    '4th',
-    style: TextStyle(
-    fontWeight: FontWeight.bold,
-    fontSize: 25.0,
-    ),
+    '$year Year',
+      style: TextStyle(
+        fontWeight: FontWeight.bold,
+        fontSize: 25.0,
+      ),
     ),
     SizedBox(height: 30.0,),
     Row(
       children: [
         Icon(Icons.email,
-        color: Colors.pink,
+          color: Colors.pink,
         ),
         SizedBox(width: 3.0,),
         Text(
         'EMAIL',
-        style: TextStyle(
-        fontSize: 20.0,
-        letterSpacing: 2.0,
-        ),
+          style: TextStyle(
+            fontSize: 20.0,
+            letterSpacing: 2.0,
+          ),
         ),
       ],
     ),
     Text(
     'tenorioaileen07@gmail.com',
-    style: TextStyle(
-    fontWeight: FontWeight.bold,
-    fontSize: 25.0,
-    ),
-    ),
+      style: TextStyle(
+        fontWeight: FontWeight.bold,
+        fontSize: 25.0,
+       ),
+      ),
     ],
     ),
     Column(
       crossAxisAlignment: CrossAxisAlignment.stretch,
       children: [
         ElevatedButton(
-          onPressed:(){},
+          onPressed:(){
+            setState(() {
+              year +=1 ;
+            });
+          },
           child: Text('Add Year',
           style: TextStyle(
             color: Colors.white,
+            fontWeight: FontWeight.bold,
+            letterSpacing: 2.0,
+            fontSize: 15.0,
           ),
           ),
           style: ButtonStyle(
-            backgroundColor: WidgetStateProperty.all(Colors.amber,)
+            backgroundColor: WidgetStateProperty.all(Colors.pink,),
           ),
         ),
       ],
@@ -122,3 +136,4 @@ child: Column(
 );
 }
 }
+
