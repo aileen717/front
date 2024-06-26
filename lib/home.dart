d570e4c86cf77862bc5c94a07b9068c1fe4adfe2
aileen717 <tenorioaileen07@gmail.com>
2024-06-26 10:36:33 +0800

    update stateful widget

--- lib/home.dart
+++ lib/home.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 class Home extends StatefulWidget {
-  const Home ({super.key});
+  const Home({super.key});
 
   @override
   State<Home> createState() => _HomeState();
@@ -10,11 +10,17 @@ class Home extends StatefulWidget {
 class _HomeState extends State<Home> {
   int year = 0;
 
- @override
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('User Profile'),
+        title: Text(
+            'User Profile',
+          style: TextStyle(
+            color: Colors.white,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
         backgroundColor: Colors.pink[500],
         centerTitle: true,
       ),
@@ -32,108 +38,122 @@ class _HomeState extends State<Home> {
                     radius: 75.0,
                   ),
                 ),
-    Divider(height: 55.0,
-    color: Colors.pink,
-    ),
-    Row(
-      children: [
-        Icon(
-            Icons.person_2,
-            color: Colors.pink,
-        ),
-        SizedBox(width: 3.0,),
-        Text(
-        'NAME',
-          style: TextStyle(
-            fontSize: 20.0,
-            letterSpacing: 2.0,
-          ),
-        ),
-      ],
-    ),
-    Text(
-      'Aileen Tenorio',
-        style: TextStyle(
-          fontWeight: FontWeight.bold,
-          fontSize: 25.0,
+                const Divider(
+                  height: 55.0,
+                  color: Colors.pink,
+                ),
+                const Row(
+                  children: [
+                    Icon(
+                      Icons.person_2,
+                      color: Colors.pink,
+                    ),
+                    SizedBox(
+                      width: 3.0,
+                    ),
+                    Text(
+                      'NAME',
+                      style: TextStyle(
+                        fontSize: 20.0,
+                        letterSpacing: 2.0,
+                      ),
+                    ),
+                  ],
+                ),
+                Text(
+                  'Aileen Tenorio',
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 25.0,
+                  ),
+                ),
+                SizedBox(
+                  height: 30.0,
+                ),
+                Row(
+                  children: [
+                    Icon(
+                      Icons.calendar_month,
+                      color: Colors.pink,
+                    ),
+                    SizedBox(
+                      width: 3.0,
+                    ),
+                    Text(
+                      'YEAR',
+                      style: TextStyle(
+                        fontSize: 20.0,
+                        letterSpacing: 2.0,
+                      ),
+                    ),
+                  ],
+                ),
+                Text(
+                  '$year Year',
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 25.0,
+                  ),
+                ),
+                SizedBox(
+                  height: 30.0,
+                ),
+                Row(
+                  children: [
+                    Icon(
+                      Icons.email,
+                      color: Colors.pink,
+                    ),
+                    SizedBox(
+                      width: 3.0,
+                    ),
+                    Text(
+                      'EMAIL',
+                      style: TextStyle(
+                        fontSize: 20.0,
+                        letterSpacing: 2.0,
+                      ),
+                    ),
+                  ],
+                ),
+                Text(
+                  'tenorioaileen07@gmail.com',
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 25.0,
+                  ),
+                ),
+              ],
+            ),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                ElevatedButton(
+                  onPressed: () {
+                    setState(() {
+                      year += 1;
+                    });
+                  },
+                  child: Text(
+                    'Add Year',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold,
+                      letterSpacing: 2.0,
+                      fontSize: 15.0,
+                    ),
+                  ),
+                  style: ButtonStyle(
+                    backgroundColor: WidgetStateProperty.all(
+                      Colors.pink,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ],
         ),
       ),
-    SizedBox(height: 30.0,),
-    Row(
-      children: [
-        Icon(Icons.calendar_month,
-            color: Colors.pink,
-        ),
-
-        SizedBox(width: 3.0,),
-        Text(
-          'YEAR',
-            style: TextStyle(
-              fontSize: 20.0,
-              letterSpacing: 2.0,
-          ),
-        ),
-      ],
-    ),
-    Text(
-    '$year Year',
-      style: TextStyle(
-        fontWeight: FontWeight.bold,
-        fontSize: 25.0,
-      ),
-    ),
-    SizedBox(height: 30.0,),
-    Row(
-      children: [
-        Icon(Icons.email,
-          color: Colors.pink,
-        ),
-        SizedBox(width: 3.0,),
-        Text(
-        'EMAIL',
-          style: TextStyle(
-            fontSize: 20.0,
-            letterSpacing: 2.0,
-          ),
-        ),
-      ],
-    ),
-    Text(
-    'tenorioaileen07@gmail.com',
-      style: TextStyle(
-        fontWeight: FontWeight.bold,
-        fontSize: 25.0,
-       ),
-      ),
-    ],
-    ),
-    Column(
-      crossAxisAlignment: CrossAxisAlignment.stretch,
-      children: [
-        ElevatedButton(
-          onPressed:(){
-            setState(() {
-              year +=1 ;
-            });
-          },
-          child: Text('Add Year',
-          style: TextStyle(
-            color: Colors.white,
-            fontWeight: FontWeight.bold,
-            letterSpacing: 2.0,
-            fontSize: 15.0,
-          ),
-          ),
-          style: ButtonStyle(
-            backgroundColor: WidgetStateProperty.all(Colors.pink,),
-          ),
-        ),
-      ],
-    ),
-  ],
-),
-),
-);
+    );
+  }
 }
-}
-
