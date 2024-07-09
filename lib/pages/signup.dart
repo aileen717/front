1fdf1abf8b346f87389442422aaf20129ea9c804
aileen717 <tenorioaileen07@gmail.com>
2024-07-09 12:06:49 +0800

    user registration

--- lib/pages/signup.dart
+++ lib/pages/signup.dart
@@ -1,4 +1,7 @@
+import 'dart:convert';
 import 'package:flutter/material.dart';
+import '../services/user.dart';
+import 'package:http/http.dart' as http;
 
 class Signup extends StatefulWidget {
   const Signup({super.key});
@@ -12,6 +15,21 @@ class _SignupState extends State<Signup> {
   String name = '';
   String email = '';
   String password = '';
+
+  createAccount(User user) async{
+    final response = await http.post (
+      Uri.parse('http://10.0.2.2:8080/api/v1/auth/register/user'),
+      headers: <String, String>{
+        'Content-Type': 'application/json; charset=UTF-8',
+      },
+      body: jsonEncode(<String, dynamic>{
+        'username': user.username,
+        'email': user.email,
+        'password': user.password,
+      }),
+    );
+    print(response.body);
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -115,9 +133,13 @@ class _SignupState extends State<Signup> {
                     ElevatedButton(onPressed: (){
                       if(formKey.currentState!.validate()){
                         formKey.currentState!.save();
-                        print(name);
-                        print(email);
-                        print(password);
+                        User user = User(
+                          username: name,
+                          email: email,
+                          password: password
+                        );
+                        createAccount(user);
+                        Navigator.pushReplacementNamed(context, '/login');
                       }
                     },
                         child: Text('Sign up',
